6d9eab1b96dba489625838b6d5bf7a1bb2afe6cd
ManojBasty <manojkumarbasty@gmail.com>
2026-02-06 21:34:53 +0530

    "feat: add route and controller structure with health check"

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
+import healthRoute from "./routes/health.route";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+// routes
+app.use("/api", healthRoute);
 
-app.get("/", (req, res) => {
-  res.send("Echo backend is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("Echo backend is running");
+// });
 
 const PORT = 5000;
 
